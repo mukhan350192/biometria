12849817242ca16c6d157f21834ae86584e3c8c9
mukhan350192 <mukhan350192@gmail.com>
2022-05-05 19:37:49 +0600

    update

--- app/Http/Controllers/BiometriaController.php
+++ app/Http/Controllers/BiometriaController.php
@@ -124,6 +124,8 @@ class BiometriaController extends Controller
             $t =  json_decode($response,true);
             $image = $t['data']['domain']['docPhoto'];
             var_dump($image);
+            $data = base64_decode($image);
+            Storage::put("test.png", $data);
             if (preg_match('/data:image\/(\w+);base64,/', $image)) {
                 $data = substr($image, strpos($image, ',') + 1);
 
