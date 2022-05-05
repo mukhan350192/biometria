cf5c604d41b3de9e45bab522a3d2ab267fcd13fa
mukhan350192 <mukhan350192@gmail.com>
2022-05-05 16:22:04 +0600

    update

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::get('/takeCode',[BiometriaController::class,'takeCode']);
+Route::get('/takeDocs',[BiometriaController::class,'takeDocs']);
 Route::get('/test',[BiometriaController::class,'test']);
