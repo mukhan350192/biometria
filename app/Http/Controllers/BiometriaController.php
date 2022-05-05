7e5deaca298b086204894279ad595e3b79d26859
mukhan350192 <mukhan350192@gmail.com>
2022-05-05 16:17:42 +0600

    update

--- app/Http/Controllers/BiometriaController.php
+++ app/Http/Controllers/BiometriaController.php
@@ -49,7 +49,8 @@ class BiometriaController extends Controller
             ]);
             $status = $result->getStatusCode();
             $response = $result->getBody()->getContents();
-            if ($status == 200 && $response->responseCode == 'PROFILE_DOCUMENT_ACCESS_SUCCESS'){
+            $response = json_decode($response, true);
+            if ($status == 200 && $response['responseCode'] == 'PROFILE_DOCUMENT_ACCESS_SUCCESS'){
                 $result['success'] = true;
                 break;
             }
@@ -61,7 +62,58 @@ class BiometriaController extends Controller
         }while(false);
         return response()->json($result);
     }
-    public function test(){
-        echo "es";
+    public function takeDocs(Request $request){
+        $code = $request->input('code');
+        $name = $request->input('name');
+        $lastName = $request->input('lastName');
+        $middleName = $request->input('middleName');
+        $iin = $request->input('iin');
+        $result['success'] = false;
+        do{
+            if (!$code){
+                $result['message'] = 'Не передан код';
+                break;
+            }
+            if (!$name){
+                $result['message'] = 'Не передан имя';
+                break;
+            }
+            if (!$middleName){
+                $result['message'] = 'Не передан фамилия';
+                break;
+            }
+            $client = new Client(['verify' => false]);
+            $response = $client->get('https://secure2.1cb.kz/fcbid-otp/api/v1/login', [
+                'headers' => [
+                    'Authorization' => 'Basic ' . base64_encode('7471656497:970908350192'),
+                    'Content-Type' => 'application/json',
+                    'Accept' => 'application/json',
+                ]
+            ]);
+            $response = $response->getBody()->getContents();
+            $response = json_decode($response, true);
+            $token = $response['access']['hash'];
+            $url = "https://secure2.1cb.kz/fcbid-otp/api/v1/get-pdf-document";
+            $uuid = Str::uuid()->toString();
+            $headers = [
+                'Authorization' => 'Bearer ' . $token,
+                'RequestID' => $uuid,
+                'Content-Type' => 'application/json',
+            ];
+            $body = [
+                'ciin' => $iin,
+                'code' => $code,
+                'last_name' => $lastName,
+                'first_name' => $name,
+                'middle_name' => $middleName,
+            ];
+            $result = $client->post($url, [
+                'headers' => $headers,
+                'body' => json_encode($body),
+            ]);
+            $response = $result->getBody()->getContents();
+            var_dump($response);
+        }while(false);
+        return response()->json($result);
     }
 }
