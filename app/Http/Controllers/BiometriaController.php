530791a447e023ccd71b3251378e1d66e70c365a
mukhan350192 <mukhan350192@gmail.com>
2022-05-26 15:22:48 +0600

    update

--- app/Http/Controllers/BiometriaController.php
+++ app/Http/Controllers/BiometriaController.php
@@ -256,7 +256,7 @@ class BiometriaController extends Controller
 
             $client = new Client(['verify' => false]);
             $s = $client->get($url);
-
+            print_r($s);
             $result['success'] = true;
             $result['similarity'] = $similarity;
 
