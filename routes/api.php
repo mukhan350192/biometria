43352b96873fbe135767bc30bce70d55012c45af
mukhan350192 <mukhan350192@gmail.com>
2022-08-05 17:02:38 +0600

    update

--- routes/api.php
+++ routes/api.php
@@ -22,6 +22,7 @@ Route::get('/takeCode',[BiometriaController::class,'takeCode']);
 Route::get('/takeDocs',[BiometriaController::class,'takeDocs']);
 Route::get('/test',[BiometriaController::class,'test']);
 Route::post('/comparePhotos',[BiometriaController::class,'comparePhotos']);
+Route::post('/compareTest',[BiometriaController::class,'compareTest']);
 Route::post('/comparePhotoManual',[BiometriaController::class,'comparePhotoManual']);
 Route::post('/upload',[BiometriaController::class,'upload']);
 Route::post('/upload',[BiometriaController::class,'upload']);
