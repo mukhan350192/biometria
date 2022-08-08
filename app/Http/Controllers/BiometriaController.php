ec385a5009df7779134e5f09aebd2055c43940c0
mukhan350192 <mukhan350192@gmail.com>
2022-08-08 13:09:09 +0600

    update

--- app/Http/Controllers/BiometriaController.php
+++ app/Http/Controllers/BiometriaController.php
@@ -628,12 +628,11 @@ class BiometriaController extends Controller
             $image = str_replace(' ', '+', $image);
             $imageName = Str::random(10) . '.jpeg';
             $first = Storage::disk('local')->put($imageName, base64_decode($image));
-            var_dump($imageName);
+
             $image = str_replace('data:image/jpeg;base64,', '', $doc);
             $image = str_replace(' ', '+', $image);
             $imageName2 = Str::random(10) . '.jpeg';
             $second = Storage::disk('local')->put($imageName2, base64_decode($image));
-            var_dump($second);
 
             $ApiKey = "PeeKMaNIX9dNL2pB2433rs7zwrs28gGZ";
             $ApiSecret = "9ab3a51f7d5acbf20fc2a778516433bb";
